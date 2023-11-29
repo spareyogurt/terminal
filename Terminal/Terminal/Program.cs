230447a2df9b7fe5a6dae013ccdf4526d0871bf5
RF <152041500+RF246@users.noreply.github.com>
2023-11-28 21:04:14 -0500

    Add files via upload

--- Terminal/Terminal/Program.cs
+++ Terminal/Terminal/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,6 +13,29 @@ namespace Terminal
         static void Main(string[] args)
         {
             Console.Title = "Terminal";
+            string ver = "1.0.0";
+            string dir = Directory.GetCurrentDirectory();
+            string pcName = Dns.GetHostName();
+            string username = Environment.UserName;
+            string input;
+            string output;
+            string temp;
+            Console.WriteLine("Terminal v" + ver);
+            while (true)
+            {
+                Console.WriteLine();
+                Console.Write(username + "@" + pcName + " / " + dir + " > ");
+                input = Console.ReadLine();
+                output = input.ToLower();
+                if (output == "help")
+                {
+                    Console.WriteLine("This command isn't finished yet. Sorry!\n:(");
+                }
+                else
+                {
+                    Console.WriteLine("This command is invalid.");
+                }
+            }
         }
     }
 }
