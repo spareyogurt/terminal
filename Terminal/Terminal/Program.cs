4c63772a6f7291450779417ec0cb48666308c03d
RF <152041500+RF246@users.noreply.github.com>
2023-12-01 20:45:09 -0500

    Add files via upload

--- Terminal/Terminal/Program.cs
+++ Terminal/Terminal/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,7 +14,7 @@ namespace Terminal
         static void Main(string[] args)
         {
             Console.Title = "Terminal";
-            string ver = "1.0.0";
+            string ver = "1.0";
             string dir = Directory.GetCurrentDirectory();
             string pcName = Dns.GetHostName();
             string username = Environment.UserName;
@@ -23,17 +24,41 @@ namespace Terminal
             Console.WriteLine("Terminal v" + ver);
             while (true)
             {
+                temp = null;
                 Console.WriteLine();
-                Console.Write(username + "@" + pcName + " / " + dir + " > ");
+                Console.Write(username + "@" + pcName + " / " + dir + "\n   > ");
                 input = Console.ReadLine();
                 output = input.ToLower();
+                Console.Title = output + " - Terminal";
                 if (output == "help")
                 {
                     Console.WriteLine("This command isn't finished yet. Sorry!\n:(");
                 }
+                else if (output == "write")
+                {
+                    temp = Console.ReadLine();
+                    Console.WriteLine("\n" + temp);
+                }
+                else if (output == "clear")
+                {
+                    Console.Clear();
+                }
+                else if (output == "directory change")
+                {
+                    temp = Console.ReadLine();
+                    if (Directory.Exists(temp))
+                    {
+                        dir = temp;
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nThis directory is invalid.");
+                    }
+                }
                 else
                 {
-                    Console.WriteLine("This command is invalid.");
+                    Console.WriteLine("\nThis command is invalid.");
+                    Console.Title = "Terminal";
                 }
             }
         }
